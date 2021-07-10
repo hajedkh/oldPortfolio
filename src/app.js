55008fd2a2f58814a9c12b8988d074b4e4d038b1
hajedkh <kh.hajed@gmail.com>
2021-07-10 10:48:48 +0200

    temporary fixed page

--- src/app.js
+++ src/app.js
@@ -6,4 +6,27 @@ function toggleButton() {
     navList.classList.toggle('show')
 }
 
+function alertNotReady(){
+    alert("this section not yet ready, it will be ready soon, download My CV from 'My Resume' down ")
+}
 hamburgerButton.addEventListener('click',toggleButton)
+
+//Get the button:
+mybutton = document.getElementById("goUp");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
